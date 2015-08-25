733b43d0699bf052ecab9191af616e9f19382740
Timothy Farrell <explorigin@gmail.com>
2015-08-25 09:40:17 -0500

    Split MainPackage to its own file.

--- src/modular/js/JsGenerator.hx
+++ src/modular/js/JsGenerator.hx
@@ -18,36 +18,6 @@ enum Forbidden {
 	constructor;
 }
 
-
-class MainPackage extends Package {
-	public override function getCode() {
-		var pre = new haxe.Template('// Package: ::packageName::
-require([::dependencyNames::],
-	    function (::dependencyVars::) {
-');
-
-		//  Collect the package's dependencies into one array
-		var allDeps = new StringMap();
-		var depKeys = [for (k in dependencies.keys()) k];
-
-		var data = {
-			packageName: name,
-			path: path,
-			dependencyNames: [for (k in depKeys) gen.api.quoteString(k.replace('.', '/'))].join(', '),
-			dependencyVars: [for (k in depKeys) k.replace('.', '_')].join(', '),
-		};
-		var _code = pre.execute(data);
-
-		_code += '\t$code';
-
-		var post = new haxe.Template('
-});
-');
-		_code += post.execute(data);
-		return _code;
-	}
-}
-
 class JsGenerator
 {
 	public var api : JSGenApi;
